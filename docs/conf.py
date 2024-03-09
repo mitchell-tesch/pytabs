a880fe9308012adb1b527dc97c7cc29a11718c89
Mitchell tesch <mitchell.z.tesch@gmail.com>
2024-03-09 23:03:48 +1000

    path clean

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,6 @@
 # Configuration file for the Sphinx documentation builder.
-import sys
-sys.path.insert(0, "../src")
-
+import sys, os
+sys.path.insert(0, os.path.abspath('../src'))
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
