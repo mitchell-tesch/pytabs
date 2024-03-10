e7a949526d50de309cbeb4d5941cc8e22ca4482c
Mitchell Tesch <mitchell.tesch@robertbird.com.au>
2024-03-10 13:10:05 +1000

    workflow test

--- src/pytabs/etabs_config.py
+++ src/pytabs/etabs_config.py
@@ -13,8 +13,9 @@ pytabs_config = config.read_config()
 # check to see if ETABS API DLL path from config file exists
 etabs_api_path = pytabs_config['ETABS']['API_DLL_PATH']
 if not os.path.isfile(etabs_api_path):
-    warnings.warn(f"ETABS API .DLL unable to be located @ {etabs_api_path}, check pytabs_config.py",
+    warnings.warn(f"ETABS API .DLL not found @ {etabs_api_path}, check pytabs_config.ini",
                   ImportWarning)
+    etabs_api_path = "./etabs/ETABSv1.dll"
 
 # pythonnet clr-loader import of Marshal - ETABS API requirement
 clr.AddReference("System.Runtime.InteropServices")
