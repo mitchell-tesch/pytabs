702ecadfce81f2fd2f0c8771490a287796486fbd
Mitchell Tesch <mitchell.tesch@robertbird.com.au>
2024-03-10 21:15:20 +1000

    standard default install path

--- src/pytabs/__init__.py
+++ src/pytabs/__init__.py
@@ -3,6 +3,6 @@ A Python wrapper for CSi ETABS .NET API - a simple yet robust Python interface
 to the ETABS .NET API. Not affiliated with Computer and Structures Inc.
 """
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 from .model import EtabsModel
